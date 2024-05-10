906eb69cf40c3bf464bee0b7d86d7542b2b348cb
Nikola Pavlović <nikolap994@gmail.com>
2024-05-10 18:54:20 +0200

    Remove old commands

--- src/helper/database.js
+++ src/helper/database.js
@@ -6,10 +6,7 @@ const connection = {};
 
 async function database() {
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(process.env.MONGODB_URI);
 
     connection.isConnected = db.connections[0].readyState;
     if (connection.isConnected) {
