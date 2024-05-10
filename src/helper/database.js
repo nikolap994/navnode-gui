c765a2d6a8bc0eace63ce6373e58e02e6cd0323f
Nikola Pavlović <nikolap994@gmail.com>
2024-05-10 18:43:08 +0200

    Add database helper

---
+++ src/helper/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+const connection = {};
+
+async function database() {
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    connection.isConnected = db.connections[0].readyState;
+    if (connection.isConnected) {
+      console.log("MongoDB database connected successfully.");
+    } else {
+      console.log("MongoDB connection failed.");
+    }
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+export default database;
