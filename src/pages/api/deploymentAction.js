798298645bf25f9aee011eba9bb2e49c40766ac0
Nikola Pavlović <nikolap994@gmail.com>
2024-05-12 20:25:57 +0200

    Update api

--- src/pages/api/deploymentAction.js
+++ src/pages/api/deploymentAction.js
@@ -1,3 +1,6 @@
+import { deploy } from "@/libs/navnode";
+import Server from "@/models/Server";
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
@@ -5,9 +8,30 @@ export default async function handler(req, res) {
 
   const { action, environment } = req.body;
 
+  const serversWithEnvironment = await Server.find({
+    "environments._id": environment,
+  });
+
+  let foundEnvironment = {};
+  let foundTask = {};
+
+  serversWithEnvironment.forEach((server) => {
+    foundEnvironment = server.environments.find(
+      (env) => env._id.toString() === environment
+    );
+  });
+
+  const serversWithTask = await Server.find({ "tasks._id": action });
+
+  serversWithTask.forEach((server) => {
+    foundTask = server.tasks.find((task) => task._id.toString() === action);
+  });
+
+  const deployResponse = await deploy(foundEnvironment, foundTask.commands);
+
   try {
     res.status(200).json({
-      message: `Action '${action}' executed successfully on '${environment}'`,
+      message: deployResponse,
     });
   } catch (error) {
     console.error("Error executing deployment action:", error);
